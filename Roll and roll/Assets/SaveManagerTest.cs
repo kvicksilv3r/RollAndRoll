98fa0c1a25f4972e1b84d245a8d10f67c661356d
Dogge <douglas.ekehammar@gmail.com>
2025-04-01 00:22:47 +0200

    Saving ugh

--- Roll and roll/Assets/SaveManagerTest.cs
+++ Roll and roll/Assets/SaveManagerTest.cs
@@ -1,6 +1,20 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+
+[System.Serializable]
+public class SaveBagFormat
+{
+    public SaveBagFormat(string diceUID, int newAmount)
+    {
+        diceID = diceUID;
+        amount = newAmount;
+    }
+
+    public string diceID;
+    public int amount;
+}
 
 public class SaveManagerTest : MonoBehaviour
 {
@@ -8,9 +22,12 @@ public class SaveManagerTest : MonoBehaviour
 
     public DiceBag myBag;
 
-    public DiceBag recreatedBag;
+    public List<SaveBagFormat> recreatedBag;
+
+    public List<SaveBagFormat> tempBag = new List<SaveBagFormat>();
 
     public bool getBag;
+    public bool getTemp;
     public bool saveBag;
     public bool loadBag;
 
@@ -38,6 +55,12 @@ public class SaveManagerTest : MonoBehaviour
             loadBag = false;
             LoadBag();
         }
+
+        if (getTemp)
+        {
+            getTemp = false;
+            tempBag = BagToSaveBag(myBag);
+        }
     }
 
     private void LoadBag()
@@ -50,12 +73,12 @@ public class SaveManagerTest : MonoBehaviour
         }
 
         var data = File.ReadAllText(path);
-        recreatedBag = JsonUtility.FromJson<DiceBag>(data);
+        recreatedBag = JsonUtility.FromJson<List<SaveBagFormat>>(data);
     }
 
     private void SaveBag()
     {
-        var data = JsonUtility.ToJson(myBag);
+        var data = JsonUtility.ToJson(tempBag);
 
         var totalPath = BasePath + "/leBag.json";
 
@@ -65,6 +88,32 @@ public class SaveManagerTest : MonoBehaviour
         }
 
         File.WriteAllText(totalPath, data);
+    }
 
+    public List<SaveBagFormat> BagToSaveBag(DiceBag bag)
+    {
+        var returnTempBag = new List<SaveBagFormat>();
+        bool found = false;
+
+        for (int i = 0; i < bag.bag.Count; i++)
+        {
+            found = false;
+
+            for (int o = 0; o < returnTempBag.Count; o++)
+            {
+                if (returnTempBag[o].diceID == bag.bag[i].UID)
+                {
+                    returnTempBag[o].amount++;
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                returnTempBag.Add(new SaveBagFormat(bag.bag[i].UID, 1));
+            }
+        }
+
+        return returnTempBag;
     }
 }
